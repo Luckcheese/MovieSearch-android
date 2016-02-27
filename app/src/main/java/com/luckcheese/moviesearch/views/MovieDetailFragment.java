2e00abe707cdce03f95cc04f455f34220587ef65
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2016-02-27 02:18:45 -0300

    Added share menu item to MovieDetailFragment

--- app/src/main/java/com/luckcheese/moviesearch/views/MovieDetailFragment.java
+++ app/src/main/java/com/luckcheese/moviesearch/views/MovieDetailFragment.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.luckcheese.moviesearch.R;
 import com.luckcheese.moviesearch.domain.FakeServer;
@@ -25,6 +29,8 @@ public class MovieDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             mItem = FakeServer.details(getArguments().getString(ARG_ITEM_ID));
@@ -46,6 +52,26 @@ public class MovieDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.movie_detail, container, false);
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.details, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.share:
+                Toast.makeText(getActivity(), getString(R.string.share_not_supported, mItem.getTitle()), Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void populateView() {
