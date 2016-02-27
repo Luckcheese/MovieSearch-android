d1cfdacdb41a5c7731c2b0f914dcebdc1142a3a4
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2016-02-26 22:52:41 -0300

    Removed unused FAB from views

--- app/src/main/java/com/luckcheese/moviesearch/views/MovieListActivity.java
+++ app/src/main/java/com/luckcheese/moviesearch/views/MovieListActivity.java
@@ -5,18 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.luckcheese.moviesearch.R;
-
 import com.luckcheese.moviesearch.views.dummy.DummyContent;
 
 import java.util.List;
@@ -45,15 +41,6 @@ public class MovieListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         View recyclerView = findViewById(R.id.movie_list);
         assert recyclerView != null;
