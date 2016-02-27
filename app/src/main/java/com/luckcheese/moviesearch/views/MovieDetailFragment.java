9eaf648f1b022af2563cce4ea267e0c0cf5f944e
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2016-02-26 22:54:45 -0300

    Removed unnecessary comments

--- app/src/main/java/com/luckcheese/moviesearch/views/MovieDetailFragment.java
+++ app/src/main/java/com/luckcheese/moviesearch/views/MovieDetailFragment.java
@@ -12,28 +12,12 @@ import android.widget.TextView;
 import com.luckcheese.moviesearch.R;
 import com.luckcheese.moviesearch.views.dummy.DummyContent;
 
-/**
- * A fragment representing a single Movie detail screen.
- * This fragment is either contained in a {@link MovieListActivity}
- * in two-pane mode (on tablets) or a {@link MovieDetailActivity}
- * on handsets.
- */
 public class MovieDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
+
     public static final String ARG_ITEM_ID = "item_id";
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
     private DummyContent.DummyItem mItem;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public MovieDetailFragment() {
     }
 
@@ -42,9 +26,6 @@ public class MovieDetailFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
@@ -60,7 +41,6 @@ public class MovieDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.movie_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.movie_detail)).setText(mItem.details);
         }
--- app/src/main/java/com/luckcheese/moviesearch/views/MovieListActivity.java
+++ app/src/main/java/com/luckcheese/moviesearch/views/MovieListActivity.java
@@ -17,20 +17,8 @@ import com.luckcheese.moviesearch.views.dummy.DummyContent;
 
 import java.util.List;
 
-/**
- * An activity representing a list of Movies. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link MovieDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- */
 public class MovieListActivity extends AppCompatActivity {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
     private boolean mTwoPane;
 
     @Override
@@ -47,10 +35,6 @@ public class MovieListActivity extends AppCompatActivity {
         setupRecyclerView((RecyclerView) recyclerView);
 
         if (findViewById(R.id.movie_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
             mTwoPane = true;
         }
     }
