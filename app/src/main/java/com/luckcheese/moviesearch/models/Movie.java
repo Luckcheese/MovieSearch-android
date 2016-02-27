bd9d8fb38eb5b7ba706d9b4dfb10c5890d11a824
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2016-02-26 23:03:40 -0300

    Added models to represent search result and detailed movies

---
+++ app/src/main/java/com/luckcheese/moviesearch/models/Movie.java
@@ -0,0 +1,105 @@
+package com.luckcheese.moviesearch.models;
+
+public class Movie {
+
+    private String title;
+    private String year;
+    private String rated;
+    private String released;
+    private String runtime;
+    private String genre;
+    private String director;
+    private String writer;
+    private String actors;
+    private String plot;
+    private String language;
+    private String country;
+    private String awards;
+    private String poster;
+    private String metascore;
+    private String imdbRating;
+    private String imdbVotes;
+    private String imdbID;
+    private String type;
+    private String response;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getRated() {
+        return rated;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getAwards() {
+        return awards;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public String getMetascore() {
+        return metascore;
+    }
+
+    public String getImdbRating() {
+        return imdbRating;
+    }
+
+    public String getImdbVotes() {
+        return imdbVotes;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+}
---
+++ app/src/main/java/com/luckcheese/moviesearch/models/MovieSearchResult.java
@@ -0,0 +1,25 @@
+package com.luckcheese.moviesearch.models;
+
+public class MovieSearchResult {
+
+    private String title;
+    private String year;
+    private String imdbID;
+    private String poster;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+}
